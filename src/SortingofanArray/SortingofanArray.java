6d16472e1aa8e9b2356a53ae96262df6b2a9f267
unknown <engr.mohammadjannatulnayeem@gmail.com>
2022-11-08 18:49:34 +0600

    sorting an array

---
+++ src/SortingofanArray/SortingofanArray.java
@@ -0,0 +1,39 @@
+package SortingofanArray;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author nayeem
+ */
+public class SortingofanArray {
+    public static void main(String[]args)
+        {
+         
+    //Ascending Order
+    //variable declaration section
+            int i;
+        
+        int[] x={3,8,1,5,2,4,7,6};
+        
+        Arrays.sort(x);
+        
+        System.out.print("Ascending Order:");
+        for(i=0;i<8;i++)
+        {
+        System.out.print(" "+x[i]);
+        
+        }
+        System.out.println("");
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        }
+}
